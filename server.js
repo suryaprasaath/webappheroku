6b5d58bae410a683052b735524705eee26fab6e8
suryaprasaath <81792262+suryaprasaath@users.noreply.github.com>
2022-01-15 18:52:53 +0530

    first commit

--- server.js
+++ server.js
@@ -1,45 +1,73 @@
-var http = require("http");
-var url = require("url");
+const express =require('express');
+const app=express();
+const bodyparser =require('body-parser');
+const Cors =require('cors');
+app.use(Cors());
 var qs = require('querystring');
-const { MongoClient } = require("mongodb");                                                                                                                                       
+var MongoClient=require('mongodb').MongoClient;                                                                                                                                       
+const { request } = require('http');
 const url1 = "mongodb+srv://surya:lakshmanan@cluster0.lmqbx.mongodb.net/test";
-const client = new MongoClient(url1);
-const dbName = "test";
-                      
- async function run(req,res) {
-         await client.connect();
-         console.log("Connected correctly to server");
-         const db = client.db(dbName);
-         const col = db.collection("people");         
-        var body ='';
-
-        req.on('data', function (data) 
-        {
-            body += data;
-        });
-
-        req.on('end', function () 
-        {
-            var post = qs.parse(body);
-            name1 = post['name'];
-            phno1 = post['phno'];
-            pass1=post['pass'];
-            email1 = post['email'];
-            cpass1=post['cpass'];
-            var MongoClient=require('mongodb');    
+const PORT=3000;
+app.use(bodyparser.json());
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials',true);
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
+app.post('/registration',function(req,res){
+    console.log(req.body);
             MongoClient.connect(url1, function(err, db)
             {
                 if(err) throw err;
                    var dbo = db.db("test");
-                   var myobj = { name: name1, email: email1, phone: phno1, password: pass1, cpassword: cpass1 };
-                   dbo.collection("people").insertOne(myobj, function(err, response)
+                   dbo.collection("people").insertOne(req.body, function(err, response)
                    {
                         if(err) throw err;
                         console.log("1 document inserted successfully");   
                         db.close();
                     });
             });
-        });
-}
-run().catch(console.dir);
-http.createServer(run).listen(7000);
+    res.status(200).send({"message":"Data received"});
+})
+app.post('/eregis',function(req,res){
+    console.log(req.body);
+            MongoClient.connect(url1, function(err, db)
+            {
+                if(err) throw err;
+                   var dbo = db.db("test");
+                   dbo.collection("eventregis").insertOne(req.body, function(err, response)
+                   {
+                        if(err) throw err;
+                        console.log("document inserted successfully");   
+                        db.close();
+                    });
+            });
+    //res.status(200).send({"message":"Data received"});
+})
+app.post('/login',function(req,res){
+    console.log(req.body);
+    console.log(req.body.Name);
+    console.log(req.body.Password);
+            MongoClient.connect(url1, function(err, db)
+            {
+                if(err) throw err;
+                   var dbo = db.db("test");
+                   dbo.collection("people").findOne({Name:req.body.Name}, function(err, response)
+                   {
+                        if(err) throw err;
+                        if (req.body.Password === response.Password){
+                            console.log('User and password is correct');
+                            res.send({"message":"positive"});
+                          } else {
+                            console.log("Credentials wrong");
+                            res.send({"message":"negative"});
+                          }         
+                    });
+            });
+    
+})
+app.listen(PORT,function(){
+    console.log("Server running");
+});
